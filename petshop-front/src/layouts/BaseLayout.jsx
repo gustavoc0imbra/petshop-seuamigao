c575e525a799c7739aeb7f18c91ebafef4694696
matheus maximiano <138736238+theusmaximiano@users.noreply.github.com>
2025-05-30 21:43:13 -0300

    Logout #0

--- petshop-front/src/layouts/BaseLayout.jsx
+++ petshop-front/src/layouts/BaseLayout.jsx
@@ -1,85 +1,114 @@
-import { AccountCircle, Login, People, Pets, ShoppingCart, ShoppingCartCheckout } from "@mui/icons-material";
-import { AppBar, Box, createTheme, Divider, Drawer, IconButton, List, ListItem, ListItemText, ThemeProvider, Toolbar, Typography } from "@mui/material";
+import {
+  AccountCircle,
+  Login,
+  People,
+  Pets,
+  ShoppingCart,
+  ShoppingCartCheckout,
+} from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  createTheme,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import { NavLink, useNavigate } from "react-router";
-import '../index.css';
+import "../index.css";
 import { getCartItems } from "../services/cartService";
 import { getAuthToken } from "../services/authService";
 
 export default function BaseLayout({ children }) {
-    const [cartOpen, setCartOpen] = useState(false);
-    const [cartProducts, setCartProducts] = useState([]);
-    const navigator = useNavigate();
-    const theme = createTheme();
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartProducts, setCartProducts] = useState([]);
+  const navigator = useNavigate();
+  const theme = createTheme();
 
-    const handleCartOpen = () => {
-        const products = getCartItems();
+  const handleCartOpen = () => {
+    const products = getCartItems();
 
-        if(products) {
-            setCartProducts(products);
-        }
-        
-        setCartOpen(true);
-    };
-
-    const handleCartClose = () => {
-        setCartOpen(false);
+    if (products) {
+      setCartProducts(products);
     }
 
-    const handleProfileClick = () => {
-        if (!getAuthToken()) {
-            navigator("/login");
-        }
-        
-        navigator("/profile");
+    setCartOpen(true);
+  };
+
+  const handleCartClose = () => {
+    setCartOpen(false);
+  };
+
+  const handleProfileClick = () => {
+    if (!getAuthToken()) {
+      navigator("/login");
     }
 
-    return (
-        <>
-            <ThemeProvider theme={theme}>
-                <AppBar component="nav" position="static">
-                    <Toolbar>
-                        <Typography flex={1}>
-                            <NavLink to={"/"} >
-                                Petshop - Seu Amigão <Pets />
-                            </NavLink>
-                        </Typography>
-                        <IconButton onClick={handleProfileClick}>
-                                <AccountCircle />
-                        </IconButton>
-                        <IconButton onClick={handleCartOpen}>
-                            <ShoppingCart />
-                        </IconButton>
-                    </Toolbar>
-                </AppBar>
-                <Drawer anchor="right" variant="temporary" open={cartOpen} onClose={handleCartClose} ModalProps={{
-                    keepMounted: true
-                }}>
-                    <Toolbar />
-                    
-                    <Box
-                        sx={{
-                            p: 4
-                        }}
-                    >
-                        <Typography>Meu Carrinho</Typography>
-                        <Divider />
-                        <List>
-                              {
-                                cartProducts.map((product) => {
-                                    return (
-                                        <ListItem>
-                                            <ListItemText>{product.name}</ListItemText>
-                                            <ListItemText>{new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(product.price)}</ListItemText>
-                                        </ListItem>
-                                    )
-                                })
-                              }
-                        </List>
-                    </Box>
-                </Drawer>
-                {children}
-            </ThemeProvider>
-        </>
-    );
+    navigator("/profile");
+  };
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <AppBar component="nav" position="static">
+          <Toolbar>
+            <Typography flex={1}>
+              <NavLink to={"/"}>
+                Petshop - Seu Amigão <Pets />
+              </NavLink>
+            </Typography>
+            <IconButton onClick={handleProfileClick}>
+              <AccountCircle />
+            </IconButton>
+            <IconButton onClick={handleCartOpen}>
+              <ShoppingCart />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          anchor="right"
+          variant="temporary"
+          open={cartOpen}
+          onClose={handleCartClose}
+          ModalProps={{
+            keepMounted: true,
+          }}
+        >
+          <Toolbar />
+
+          <Box
+            sx={{
+              p: 4,
+            }}
+          >
+            <Typography>Meu Carrinho</Typography>
+            <Divider />
+            <List>
+              {cartProducts.map((product) => {
+                return (
+                  <ListItem>
+                    <ListItemText>{product.name}</ListItemText>
+                    <ListItemText>
+                      {new Intl.NumberFormat("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                      }).format(product.price)}
+                    </ListItemText>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Box>
+        </Drawer>
+        {children}
+      </ThemeProvider>
+    </>
+  );
 }
--- petshop-front/src/services/authService.js
+++ petshop-front/src/services/authService.js
@@ -3,37 +3,42 @@ import api from "./api";
 const API_URL = "http://localhost:8081/api/v0/auth";
 
 export async function login(credentials) {
-    try {
-        const response = await api.post(API_URL, credentials);
+  try {
+    const response = await api.post(API_URL, credentials);
 
-        return { success: true, body: { data: response.data } };
-    }catch(e) {
-        console.log(e)
-        return { success: false, body: { message: e.response } };
-    }
+    return { success: true, body: { data: response.data } };
+  } catch (e) {
+    console.log(e);
+    return { success: false, body: { message: e.response } };
+  }
 }
 
 export async function auth(token) {
-    try {
-        const response = await api.post(`${API_URL}/validate`, null, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
+  try {
+    const response = await api.post(`${API_URL}/validate`, null, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-        return { success: true, body: { data: response.data } };
-    }catch(e) {
-        console.log(e)
-        return { success: false, body: { message: e.response } };
-    }
+    return { success: true, body: { data: response.data } };
+  } catch (e) {
+    console.log(e);
+    return { success: false, body: { message: e.response } };
+  }
 }
 
 export function getAuthToken() {
-    const token = localStorage.getItem('token');
-    return token ? token : null;
+  const token = localStorage.getItem("token");
+  return token ? token : null;
 }
 
 export function getAuthUser() {
-    const user = localStorage.getItem('user');
-    return user ? JSON.parse(user) : null;
+  const user = localStorage.getItem("user");
+  return user ? JSON.parse(user) : null;
+}
+
+export function removeAuthToken() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
 }
